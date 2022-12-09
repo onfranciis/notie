7706b524de4e8c7a04828b0bef68f2ebb53eb5f8
onfranciis <96952305+onfranciis@users.noreply.github.com>
2022-12-09 07:49:10 +0100

    Added Top Icon Decider

--- components/Top.jsx
+++ components/Top.jsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import { MdEdit } from "react-icons/md";
+import { IoMdCheckmark } from "react-icons/io";
+import { BsInfoLg } from "react-icons/bs";
+import { TopIconDecider } from "./TopIconDecider";
 
-const Top = () => {
-  const Logo = 0;
+const Top = ({ Mode, setMode, setMetaTitle, setSave }) => {
+  const Logo = 10;
 
   return (
     <div className=" items-center grid grid-cols-3 py-4">
@@ -12,12 +15,16 @@ const Top = () => {
           src="/Notie_Logo2.svg"
           height={85 + Logo}
           width={208 + Logo}
+          onClick={() => setMode("View")}
         />
       </div>
-      <div className="flex justify-center">
-        <div className="bg-primary h-fit w-fit p-1 rounded-lg hover:bg-primary2 transition-all duration-100 col-start-3 ">
-          <MdEdit size="1.7em" title="New Note" className="text-tertiary " />
-        </div>
+      <div className="flex justify-center gap-2">
+        <TopIconDecider
+          Mode={Mode}
+          setMode={(data) => setMode(data)}
+          setMetaTitle={(data) => setMetaTitle(data)}
+          setSave={setSave}
+        />
       </div>
     </div>
   );
---
+++ components/TopIconDecider.js
@@ -0,0 +1,53 @@
+import { BsInfoLg } from "react-icons/bs";
+import { IoMdCheckmark } from "react-icons/io";
+import { MdEdit } from "react-icons/md";
+
+export const TopIconDecider = ({ Mode, setMode, setMetaTitle, setSave }) => {
+  if (Mode == "View") {
+    return (
+      <div
+        className="bg-primary h-fit w-fit p-1 rounded-lg hover:bg-primary2 transition-all duration-100 col-start-3 "
+        onClick={() => {
+          setMode("Edit");
+        }}
+      >
+        <MdEdit size="1.7em" title="Done" className="text-tertiary " />
+      </div>
+    );
+  } else if (Mode == "Edit") {
+    return (
+      <div
+        className="bg-primary h-fit w-fit p-1 rounded-lg hover:bg-primary2 transition-all duration-100 col-start-3 "
+        onClick={() => {
+          setMode("View");
+          setMetaTitle("Notie By Onfranciis");
+          setSave();
+        }}
+      >
+        <IoMdCheckmark size="1.7em" title="Done" className="text-tertiary " />
+      </div>
+    );
+  } else if (Mode == "Preview") {
+    return (
+      <>
+        <div
+          className="bg-primary h-fit w-fit p-1 rounded-lg hover:bg-primary2 transition-all duration-100 col-start-3 "
+          onClick={() => {
+            setMode("Edit");
+          }}
+        >
+          <MdEdit size="1.7em" title="Done" className="text-tertiary " />
+        </div>
+
+        <div
+          className="bg-primary h-fit w-fit p-1 rounded-lg hover:bg-primary2 transition-all duration-100 col-start-3 "
+          onClick={() => {
+            setMode("Info");
+          }}
+        >
+          <BsInfoLg size="1.7em" title="Done" className="text-tertiary " />
+        </div>
+      </>
+    );
+  }
+};
