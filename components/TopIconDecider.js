fa7ca5e2183fa96a863ba7e7e3cb88cba3e1d2ac
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-01-18 21:45:15 +0100

    fix: add more support for EditNote

--- components/TopIconDecider.js
+++ components/TopIconDecider.js
@@ -20,7 +20,7 @@ export const TopIconDecider = ({
         <MdEdit size="1.7em" title="Add New Note" className="text-tertiary " />
       </div>
     );
-  } else if (Mode == "Edit" || Mode == "EditNote") {
+  } else if (Mode == "Edit") {
     return (
       <div
         className="bg-primary h-fit w-fit p-1 rounded-lg hover:bg-primary2 transition-all duration-100 col-start-3 "
@@ -33,13 +33,26 @@ export const TopIconDecider = ({
         <IoMdCheckmark size="1.7em" title="Done" className="text-tertiary " />
       </div>
     );
+  } else if (Mode == "EditNote") {
+    return (
+      <div
+        className="bg-primary h-fit w-fit p-1 rounded-lg hover:bg-primary2 transition-all duration-100 col-start-3 "
+        onClick={() => {
+          setMode("Preview");
+          setMetaTitle("Notie By Onfranciis");
+          setSave();
+        }}
+      >
+        <IoMdCheckmark size="1.7em" title="Update" className="text-tertiary " />
+      </div>
+    );
   } else if (Mode == "Preview") {
     return (
       <>
         <div
           className="bg-primary h-fit w-fit p-1 rounded-lg hover:bg-primary2 transition-all duration-100 col-start-3 "
           onClick={() => {
-            setMode("Edit");
+            setMode("EditNote");
           }}
         >
           <MdEdit size="1.7em" title="Done" className="text-tertiary " />
