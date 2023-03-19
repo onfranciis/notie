b7f987175f4518743f48db6799e807a71fe527d5
onfranciis <96952305+onfranciis@users.noreply.github.com>
2023-03-20 00:55:19 +0100

    feat: add delete feature

--- components/Info.jsx
+++ components/Info.jsx
@@ -1,13 +1,26 @@
-import { receiveNote } from "./HandleNote";
+import { MdDelete } from "react-icons/md";
+import { deleteNote, receiveNote } from "./HandleNote";
 
-const Info = ({ removeInfo, Index, setInfo }) => {
+const Info = ({ Index, setIndex, setInfo }) => {
   const { Created, Date, Body, Title } = receiveNote()[Index];
   return (
     <div
       className="fixed h-screen w-screen top-0 left-0 flex flex-row justify-start items-end px-5 py-7 transition"
       onClick={() => setInfo(false)}
     >
-      <div className=" flex-col bg-[white] max-h-[90vh] w-[25rem] py-2 px-4 rounded-lg drop-shadow-[0_0_5px_black] overflow-scroll">
+      <div className=" flex-col bg-[white] max-h-[90vh] w-[25rem] py-2 px-4 rounded-lg drop-shadow-[0_0_5px_black] overflow-visible  relative">
+        <div
+          className="bg-primary h-fit w-fit max-w-fit p-1 rounded-lg hover:bg-primary2 transition-all duration-100 col-start-3 absolute right-[-15px] top-[-12px]"
+          onClick={(e) => {
+            e.stopPropagation();
+            setInfo(false);
+            setIndex(false);
+            deleteNote(Index);
+          }}
+        >
+          <MdDelete size="1.5em" title="Delete" className="text-tertiary " />
+        </div>
+
         <p className="text-center text-lg font-bold overflow-scroll max-h-[50vh]">
           {Title}
         </p>
